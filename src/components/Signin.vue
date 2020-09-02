7ab985f92f0bf146e7cae26d5f520795f13d7284
nikororo <nikoro52@gmail.com>
2020-09-02 14:14:36 +0700

    fix activity

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,20 +1,20 @@
 <template>
 	<div class="authorization">
-    <img alt="Okr" :src="`${publicPath}auth.png`">
+		<img alt="Okr" :src="`${publicPath}auth.png`">
 		<form v-on:submit.prevent="onLog">
 			<div class="authTitle">Вход</div>
-      <label for="signinEmail">E-mail</label>
+			<label for="signinEmail">E-mail</label>
 			<input id="signinEmail" v-model="email" v-bind:class="{error: this.$store.state.authHasError}" type="email" placeholder="E-mail" required/>
-      <label for="signinPass">Пароль</label>
+			<label for="signinPass">Пароль</label>
 			<input id="signinPass" v-model="password" v-bind:class="{error: this.$store.state.authHasError}" type="password" placeholder="Пароль" required/>
-			<div class="errorMsg" v-if="this.$store.state.authHasError">Неверный email или пароль</div>
-      <div class="message">Еще не зарегистрированы? <router-link to="/signup">Создать аккаунт</router-link></div>
-      <div class="enterGoogle"> 
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0.850382 4.39371C2.1614 1.78937 4.85435 0 7.97252 0C10.1251 0 11.9234 0.788397 13.3052 2.07285L11.0552 4.26974C10.4794 3.72934 9.46956 3.08269 7.97252 3.08269C5.86424 3.08269 4.07487 4.47347 3.42815 6.39573C3.2687 6.89179 3.1713 7.42327 3.1713 7.97252C3.1713 8.52171 3.2687 9.05325 3.43701 9.54931C4.07487 11.4716 5.86424 12.8623 7.97252 12.8623C9.15951 12.8623 10.0631 12.5345 10.7363 12.065C11.7993 11.321 12.2777 10.2137 12.3662 9.47841H7.97252V6.51976H15.4578C15.5729 7.01582 15.6261 7.49417 15.6261 8.14968C15.6261 10.5769 14.758 12.6231 13.2521 14.0139C11.9322 15.2363 10.1251 15.945 7.97252 15.945C4.85435 15.945 2.1614 14.1556 0.850382 11.5513C0.310046 10.4705 0 9.25698 0 7.97252C0 6.68806 0.310046 5.47444 0.850382 4.39371Z" fill="black"/>
-        </svg>
-        <div class="message">Войдите при помощи аккаунта Google</div>
-      </div>
+			<div class="errorMsg" v-if="this.$store.state.authHasError">Неверно введен email и/или пароль.</div>
+			<div class="message">Еще не зарегистрированы? <router-link to="/signup">Создать аккаунт</router-link></div>
+			<div class="enterGoogle"> 
+				<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M0.850382 4.39371C2.1614 1.78937 4.85435 0 7.97252 0C10.1251 0 11.9234 0.788397 13.3052 2.07285L11.0552 4.26974C10.4794 3.72934 9.46956 3.08269 7.97252 3.08269C5.86424 3.08269 4.07487 4.47347 3.42815 6.39573C3.2687 6.89179 3.1713 7.42327 3.1713 7.97252C3.1713 8.52171 3.2687 9.05325 3.43701 9.54931C4.07487 11.4716 5.86424 12.8623 7.97252 12.8623C9.15951 12.8623 10.0631 12.5345 10.7363 12.065C11.7993 11.321 12.2777 10.2137 12.3662 9.47841H7.97252V6.51976H15.4578C15.5729 7.01582 15.6261 7.49417 15.6261 8.14968C15.6261 10.5769 14.758 12.6231 13.2521 14.0139C11.9322 15.2363 10.1251 15.945 7.97252 15.945C4.85435 15.945 2.1614 14.1556 0.850382 11.5513C0.310046 10.4705 0 9.25698 0 7.97252C0 6.68806 0.310046 5.47444 0.850382 4.39371Z" fill="black"/>
+				</svg>
+				<div class="message">Войдите при помощи аккаунта Google</div>
+			</div>
 			<button class="submit">Войти</button>
 		</form>
 	</div>
@@ -27,21 +27,21 @@ export default {
 	data: () => ({
 		email: '',
 		password: '',
-    publicPath: process.env.BASE_URL
-  }),
+		publicPath: process.env.BASE_URL
+	}),
 
-  methods: {
-    onLog() {
+	methods: {
+		onLog() {
 			//ВЫЗВАТЬ АВТОРИЗАЦИЮ С БЕКА,
 			// .then(() => {
 			// 	this.$store.commit('authCorr');
-      //  this.$router.push('/');
+			//  this.$router.push('/');
 			// })
 			// .catch(() => {
 			// 		this.$store.commit('authErr');
 			// });
-    },
-  }
+		},
+	}
 }
 </script>
 
@@ -54,77 +54,77 @@ export default {
 }
 
 .authorization form {
-  min-width: 325px;
-  max-width: 325px;
-  padding-right: 45px;
-  margin-left: 130px;
+	min-width: 325px;
+	max-width: 325px;
+	padding-right: 45px;
+	margin-left: 130px;
 }
 
 .authorization .authTitle {
-  margin-bottom: 25px;
-  font-size: 40px;
+	margin-bottom: 25px;
+	font-size: 40px;
 }
 
 .authorization input, .authorization select {
-  width: 100%;
-  height: 46px;
-  border: 2px solid #dddddd;
-  margin: 5px 0 15px;
-  padding-left: 15px;
-  box-sizing: border-box;
+	width: 100%;
+	height: 46px;
+	border: 2px solid #dddddd;
+	margin: 5px 0 15px;
+	padding-left: 15px;
+	box-sizing: border-box;
 }
 
 .authorization label {
-  margin-left: 17px;
+	margin-left: 17px;
 }
 
 .authorization .error {
-  border: 1.5px solid rgb(255, 112, 112);
+	border: 1.5px solid rgb(255, 112, 112);
 }
 
 .authorization .errorMsg {
-  color: #ff3838;
-  text-align: left;
-  margin-bottom: 15px;
-  font-size: 14px;
+	color: #ff3838;
+	text-align: left;
+	margin-bottom: 15px;
+	font-size: 14px;
 }
 
 .authorization button {
-  outline: 0;
-  border-radius: 19px;
-  background-color: #3ec6ff;
-  border: none;
-  min-width: 200px;
-  padding: 4px 25px;
-  display: block;
-  margin: 0 auto;
-  color: #fff;
-  font-size: 22px;
+	outline: 0;
+	border-radius: 19px;
+	background-color: #3ec6ff;
+	border: none;
+	min-width: 200px;
+	padding: 4px 25px;
+	display: block;
+	margin: 0 auto;
+	color: #fff;
+	font-size: 22px;
 }
 
 .authorization button:hover {
-  background-color: #00adf7;
+	background-color: #00adf7;
 }
 
 .authorization .message {
-  font-size: 14px;
-  margin-top: 10px;
-  text-align: center;
+	font-size: 14px;
+	margin-top: 10px;
+	text-align: center;
 }
 
 .authorization .enterGoogle {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 	justify-content: center;
-  margin: 25px 0;
+	margin: 25px 0;
 }
 
 .enterGoogle .message {
-  margin: 0 0 0 10px;
+	margin: 0 0 0 10px;
 }
 
 .authorization .message a {
-  color: #86919a;
+	color: #86919a;
 }
 </style>
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -5,31 +5,35 @@
 			<div class="authTitle">Регистрация</div>
 			<label for="signupFullName">Полное имя</label>
 			<input id="signupFullName" v-model="fullName" type="text" placeholder="Полное имя" required/>
-      <label for="signupEmail">E-mail</label>
+			<label for="signupEmail">E-mail</label>
 			<input id="signupEmail" v-model="email" v-bind:class="{error: this.$store.state.authHasError}" type="email" placeholder="E-mail" required/>
-      <label for="signupCompany">Название вашей компании</label>
+			<label for="signupCompany">Название вашей компании</label>
 			<input id="signupCompany" v-model="company" type="text" placeholder="Название компании" required/>
-      <label for="signupActivity">Ваша сфера деятельности</label>
+			<label for="signupActivity">Ваша сфера деятельности</label>
 			<div>
 				<select id="signupActivity" v-model="activity" required>
 					<option value disabled selected>Сфера деятельности</option>
-					<option value="it">IT</option>
-					<option selected value="marketing">Маркетинг</option>
-					<option value="hr">Кадровая служба</option>
+					<option value="departmentHead">Руководитель отдела</option>
+					<option value="manager">Менеджер</option>
+					<option selected value="developer">Разработчик</option>
+					<option value="analyst">Аналитик</option>
+					<option value="designer">Дизайнер</option>
+					<option selected value="tester">Тестировщик</option>
+					<option value="other">Другое</option>
 				</select>
 			</div>
-      <label for="signupPass">Пароль</label>
+			<label for="signupPass">Пароль</label>
 			<input id="signupPass" v-model="password" v-bind:class="{error: this.$store.state.authHasError}" type="password" placeholder="Пароль" required/>
-      <div class="errorMsg" v-if="this.$store.state.authHasError">{{this.$store.state.errMsg}}</div>
+			<div class="errorMsg" v-if="this.$store.state.authHasError">{{this.$store.state.errMsg}}</div>
 			<div class="message">Уже зарегистрированы? <router-link to="/signin">Войти</router-link></div>
 			<div class="enterGoogle"> 
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0.850382 4.39371C2.1614 1.78937 4.85435 0 7.97252 0C10.1251 0 11.9234 0.788397 13.3052 2.07285L11.0552 4.26974C10.4794 3.72934 9.46956 3.08269 7.97252 3.08269C5.86424 3.08269 4.07487 4.47347 3.42815 6.39573C3.2687 6.89179 3.1713 7.42327 3.1713 7.97252C3.1713 8.52171 3.2687 9.05325 3.43701 9.54931C4.07487 11.4716 5.86424 12.8623 7.97252 12.8623C9.15951 12.8623 10.0631 12.5345 10.7363 12.065C11.7993 11.321 12.2777 10.2137 12.3662 9.47841H7.97252V6.51976H15.4578C15.5729 7.01582 15.6261 7.49417 15.6261 8.14968C15.6261 10.5769 14.758 12.6231 13.2521 14.0139C11.9322 15.2363 10.1251 15.945 7.97252 15.945C4.85435 15.945 2.1614 14.1556 0.850382 11.5513C0.310046 10.4705 0 9.25698 0 7.97252C0 6.68806 0.310046 5.47444 0.850382 4.39371Z" fill="black"/>
-        </svg>
-        <div class="message">Зарегистрироваться через Google</div>
-      </div>
-      <button>Зарегистрироваться</button>
-    </form>
+				<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M0.850382 4.39371C2.1614 1.78937 4.85435 0 7.97252 0C10.1251 0 11.9234 0.788397 13.3052 2.07285L11.0552 4.26974C10.4794 3.72934 9.46956 3.08269 7.97252 3.08269C5.86424 3.08269 4.07487 4.47347 3.42815 6.39573C3.2687 6.89179 3.1713 7.42327 3.1713 7.97252C3.1713 8.52171 3.2687 9.05325 3.43701 9.54931C4.07487 11.4716 5.86424 12.8623 7.97252 12.8623C9.15951 12.8623 10.0631 12.5345 10.7363 12.065C11.7993 11.321 12.2777 10.2137 12.3662 9.47841H7.97252V6.51976H15.4578C15.5729 7.01582 15.6261 7.49417 15.6261 8.14968C15.6261 10.5769 14.758 12.6231 13.2521 14.0139C11.9322 15.2363 10.1251 15.945 7.97252 15.945C4.85435 15.945 2.1614 14.1556 0.850382 11.5513C0.310046 10.4705 0 9.25698 0 7.97252C0 6.68806 0.310046 5.47444 0.850382 4.39371Z" fill="black"/>
+				</svg>
+				<div class="message">Зарегистрироваться через Google</div>
+			</div>
+			<button>Зарегистрироваться</button>
+		</form>
 	</div>
 </template>
 
@@ -43,20 +47,20 @@ export default {
 		company: '',
 		fullName: '',
 		activity: '',
-    publicPath: process.env.BASE_URL
-  }),
-  
-  methods: {
-    onReg() {
+		publicPath: process.env.BASE_URL
+	}),
+	
+	methods: {
+		onReg() {
 			//ВЫЗВАТЬ регистрацию С БЕКА,
 			// .then(() => {
 			// 	this.$store.commit('authCorr');
-      //  this.$router.push('/');
+			//  this.$router.push('/');
 			// })
 			// .catch(() => {
 			// 		this.$store.commit('authErr');
 			// });
-    },
-  }
+		},
+	}
 }
 </script>
